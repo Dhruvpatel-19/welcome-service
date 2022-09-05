3968a2a1376198647a5671081ffeb694abf0755e
Jay Rathod <jay.rathod@simformsolutions.com>
2022-09-05 14:23:01 +0530

    added Swagger

--- src/main/java/com/example/welcomeservice/controller/UserController.java
+++ src/main/java/com/example/welcomeservice/controller/UserController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping(value = "/callWelcomeService/user")
@@ -15,16 +18,43 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+
+
+    @Operation(summary = "Create user",description = "This method is used to create User", tags = {"UserController"})
+    @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "User Created"),
+      @ApiResponse(responseCode = "400",description = "Bad Request"),
+      @ApiResponse(responseCode = "404",description = "Property not found"),
+      @ApiResponse(responseCode = "403",description = "Forbidden")
+    })
     @PostMapping(value = "/register")
     public User saveUser(@RequestBody User user){
         return userService.saveUser(user);
     }
 
+
+
+    @Operation(summary = "Find All User",description = "This method is used to get all the User", tags = {"UserController"})
+    @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Found all users"),
+      @ApiResponse(responseCode = "400",description = "Bad Request"),
+      @ApiResponse(responseCode = "404",description = "Property not found"),
+      @ApiResponse(responseCode = "403",description = "Forbidden")
+    })
     @GetMapping(value = "/findAll")
     public List<User> findAllUser(){
         return userService.findAllUser();
     }
 
+
+
+    @Operation(summary = "Update User",description = "This method is used to update details of the user", tags = {"UserController"})
+    @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Updated the user"),
+      @ApiResponse(responseCode = "400",description = "Bad Request"),
+      @ApiResponse(responseCode = "404",description = "Property not found"),
+      @ApiResponse(responseCode = "403",description = "Forbidden")
+    })
     @PutMapping(value = "/update/{id}")
     public UserDTO updateUser(@PathVariable("id") int id ,@RequestBody User updatedUser){
         return userService.updateUser(id,  updatedUser);
--- src/main/java/com/example/welcomeservice/controller/WelcomeController.java
+++ src/main/java/com/example/welcomeservice/controller/WelcomeController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/welcomeService/")
@@ -18,11 +21,26 @@ public class WelcomeController {
     @Autowired
     private WelcomeService welcomeService;
 
+    @Operation(summary = "Get All Favourite Property From User Favourite List",description = "This method is used to get all the favourite property of the user", tags = {"WelcomeController"})
+    @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Reteived all property successfully"),
+      @ApiResponse(responseCode = "400",description = "Bad Request"),
+      @ApiResponse(responseCode = "404",description = "Property not found"),
+      @ApiResponse(responseCode = "403",description = "Forbidden")
+    })
     @GetMapping(value = "/getAllProperty")
     public List<AllPropertyDTO> getAllProperty(){
         return welcomeService.getAllProperty();
     }
 
+
+    @Operation(summary = "Buy the property",description = "This method is used to buy a particular property by the user.", tags = {"WelcomeController"})
+    @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Brought the property successfully"),
+      @ApiResponse(responseCode = "400",description = "Bad Request"),
+      @ApiResponse(responseCode = "404",description = "Property not found"),
+      @ApiResponse(responseCode = "403",description = "Forbidden")
+    })
     @GetMapping(value = "/buyProperty/{id}")
     public String buyProperty(HttpServletRequest request ,  @PathVariable("id") int id) throws Exception {
         return welcomeService.buyProperty(request, id);
